a316df6e1e4d10f10ff6c521c212b57bd8c100b9
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-05-03 19:21:30 +0530

    Commit

--- app/src/main/java/com/example/shigi/MainActivity.kt
+++ app/src/main/java/com/example/shigi/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.in_id)
         val text : TextView = findViewById(R.id.in_sign_up)
+        val button : EditText = findViewById(R.id.in_id)
 
         button.setOnClickListener(){
-            val intent = Intent(this, listDoctor::class.java)
+            intent = Intent(this, listDoctor::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/shigi/doctorStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shigi
-
-data class doctorStatus(
-        val id: Int,
-        val name: String,
-        val speciality : String,
-        val availability : Int,
-        val image : Int
-)
