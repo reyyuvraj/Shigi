d90366f280c87c8317b0753eb863af48bc86eb64
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-05-05 14:50:24 +0530

    Commit

--- app/src/main/java/com/example/shigi/activity/DocDetail.kt
+++ app/src/main/java/com/example/shigi/activity/DocDetail.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
